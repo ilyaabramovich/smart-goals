757977255da1101e295d9bcadc42e71c3faf28cb
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-07 12:36:31 +0300

    feat: add application level error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+  private
+
+  def record_not_found(errors)
+    render json: { error: errors.message }, status: :not_found
+  end
+
+  def invalid_record(invalid_record)
+    render json: { error: invalid_record.record.errors }, status: :unprocessable_entity
+  end
 end
