d537bcb1e1bb9d9c767e4e7fdd5b0bdba49c1dfb
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-08 17:46:33 +0300

    refactor: use jsx style

--- smart-goals-client/src/index.js
+++ smart-goals-client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import { action as destroyAction } from './routes/destroy'
@@ -11,39 +11,37 @@ import Goal, { loader as goalLoader } from './routes/goal'
 import EditGoal, { action as editAction } from './routes/edit'
 import Index from './routes'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    loader: rootLoader,
-    action: rootAction,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Index /> },
-          {
-            path: 'goals/:goalId',
-            element: <Goal />,
-            loader: goalLoader,
-          },
-          {
-            path: 'goals/:goalId/edit',
-            element: <EditGoal />,
-            loader: goalLoader,
-            action: editAction,
-          },
-          {
-            path: 'goals/:goalId/destroy',
-            action: destroyAction,
-            errorElement: <div>An error occured while trying to delete a goal.</div>,
-          },
-        ],
-      },
-    ],
-  },
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="goals/:goalId"
+          element={<Goal />}
+          loader={goalLoader}
+        />
+        <Route
+          path="goals/:goalId/edit"
+          element={<EditGoal />}
+          loader={goalLoader}
+          action={editAction}
+        />
+        <Route
+          path="goals/:goalId/destroy"
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
