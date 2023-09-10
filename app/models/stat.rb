2bf51d1bf287562b630ad485efb7cbd9476fa39b
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-10 22:52:36 +0300

    allow measurement_value to be nil

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,7 +1,7 @@
 class Stat < ApplicationRecord
   belongs_to :goal
 
-  validates :measurement_value, numericality: { greater_than_or_equal_to: 0 }
+  validates :measurement_value, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   validates :measurement_date, presence: true
 
   scope :prior_to_date, ->(date) { where(measurement_date: ...date) }
