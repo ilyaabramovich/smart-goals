92fc5ade0c43207ca5f292b283a7a983a3357f7f
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-16 23:34:57 +0300

    remove date-fns

--- smart-goals-client/src/utils/parseDate.js
+++ smart-goals-client/src/utils/parseDate.js
@@ -1,5 +1,3 @@
-import { format, parseISO } from 'date-fns'
-
 export function formatDateString(dateString) {
-  return format(parseISO(dateString), 'yyyy-MM-dd')
+  return new Date(dateString).toLocaleDateString('fr-CA') // yyyy-MM-dd
 }
