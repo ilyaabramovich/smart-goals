fe7cd9a4459a9c5d42d18bd59c1e763a59c70d10
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-18 23:34:56 +0300

    feat: add prior_to_date scope tests

--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -4,4 +4,16 @@ FactoryBot.define do
     target_value { 5 }
     target_date { 1.week.from_now }
   end
+
+  trait :without_create_time_frame_stats_callback do
+    after(:build) do
+      Goal.skip_callback(:create, :after, :create_time_frame_stats)
+    end
+
+    to_create { |instance| instance.save!(validate: false) }
+
+    after(:create) do
+      Goal.set_callback(:create, :after, :create_time_frame_stats)
+    end
+  end
 end
--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -10,6 +10,21 @@ RSpec.describe Stat, type: :model do
     it { is_expected.to validate_numericality_of(:measurement_value).is_greater_than_or_equal_to 0 }
   end
 
+  describe 'scopes' do
+    describe '.prior_to_date' do
+      it 'returns goals with measurement_dates that are less than or equal to specified date' do
+        user = create(:user)
+        goal = create(:goal, :without_create_time_frame_stats_callback, user: user)
+        future_stat = create(:stat, measurement_date: 1.week.from_now, goal: goal)
+        stat_due_yesterday = create(:stat, measurement_date: 1.day.before, goal: goal)
+        time_frame = Time.current.beginning_of_day
+        stat_due_today = create(:stat, measurement_date: time_frame, goal: goal)
+
+        expect(Stat.prior_to_date(time_frame).ids).to match_array([stat_due_yesterday.id, stat_due_today.id])
+      end
+    end
+  end
+
   describe '#measured?' do
     let(:user) { build(:user) }
     let(:goal) { build(:goal, user: user) }
