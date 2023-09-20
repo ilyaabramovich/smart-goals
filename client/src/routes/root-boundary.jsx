e9866e18e0b24e4b97dd37395ab601c68caa765a
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-20 16:39:04 +0300

    feat: style RootBoundary

--- client/src/routes/root-boundary.jsx
+++ client/src/routes/root-boundary.jsx
@@ -1,17 +1,48 @@
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert'
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
+import Header from '../components/header'
 
 export default function RootBoundary() {
-  const error = useRouteError();
+  const error = useRouteError()
 
-  if (isRouteErrorResponse(error)) {
-    if (error.status === 404) {
-      return <div>This page does not exist!</div>;
+  const getErrorMessage = (err) => {
+    if (isRouteErrorResponse(err)) {
+      if (err.status === 404) {
+        return 'This page does not exist!'
+      }
+
+      if (err.status === 401) {
+        return 'You are not authorized to see this'
+      }
     }
 
-    if (error.status === 401) {
-      return <div>You are not authorized to see this</div>;
+    if (err instanceof Error) {
+      return err.message
     }
+
+    return 'Something went wrong'
   }
 
-  return <div>Something went wrong</div>;
+  const getErrorStack = (err) => {
+    if (isRouteErrorResponse(err)) {
+      return err.error?.stack
+    }
+    return err?.stack
+  }
+
+  return (
+    <>
+      <Header />
+      <Container as="main" className="my-4">
+        <h1 className="fs-4 text-danger">Application error</h1>
+        <Alert className="mb-2" variant="danger">
+          {getErrorMessage(error)}
+        </Alert>
+        <code className="selectable" style={{ wordBreak: 'break-word' }}>
+          {getErrorStack(error)}
+        </code>
+      </Container>
+    </>
+  )
 }
