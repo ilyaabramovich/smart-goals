3efc96b1ead941e23c8d6e17748827eba99201d6
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-20 16:38:36 +0300

    refactor: extract header component

---
+++ client/src/components/header.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import { Link } from 'react-router-dom'
+import Navigation from '../components/navigation'
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar expand="sm" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            SMART goals
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end">
+            <Navigation />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
--- client/src/routes/layout.jsx
+++ client/src/routes/layout.jsx
@@ -1,22 +1,11 @@
 import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import { Link, Outlet } from 'react-router-dom'
-import Navigation from '../components/navigation'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/header'
 
 export default function Layout() {
   return (
     <>
-      <Navbar expand="sm" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            SMART goals
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className="justify-content-end">
-            <Navigation />
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Header />
       <Container as="main" className="my-4">
         <Outlet />
       </Container>
