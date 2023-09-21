40c6e1a38703d7f7822820124f94d97b271355ea
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-21 13:29:11 +0300

    fix: class to className

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="flex flex-wrap items-center mx-auto max-w-screen-xl p-4">
-          <Link to="/" class="flex items-center mr-6">
+          <Link to="/" className="flex items-center mr-6">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SMART goals</span>
           </Link>
           <Navigation />
