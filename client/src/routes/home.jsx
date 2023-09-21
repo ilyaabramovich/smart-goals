0b118ac82b260bded94dde2433fb2c20b25e094d
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-21 15:06:48 +0300

    feat: add meta desription

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="fs-4">
-      This app allows you to create SMART goals, track your progress and achieve success in life.
+      Create SMART goals, track your progress and achieve success in life.
     </h1>
   )
 }
