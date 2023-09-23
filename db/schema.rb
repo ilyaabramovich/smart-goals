6c1fbc92f81fb70119cfbc22af214ee94a956bad
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-23 18:27:01 +0300

    feat: migrate to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_094603) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_144351) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "goals", force: :cascade do |t|
     t.datetime "target_date"
     t.float "target_value"
@@ -19,12 +22,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_094603) do
     t.float "initial_value", default: 0.0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "stats", force: :cascade do |t|
-    t.integer "goal_id", null: false
+    t.bigint "goal_id", null: false
     t.float "measurement_value"
     t.datetime "measurement_date"
     t.datetime "created_at", null: false
@@ -37,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_094603) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "goals", "users"
