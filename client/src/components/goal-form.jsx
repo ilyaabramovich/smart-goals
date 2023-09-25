50fb588cd7f186aeff9cca7870d2b08e28025a9c
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-25 17:06:03 +0300

    feat(goal): form layout update

--- client/src/components/goal-form.jsx
+++ client/src/components/goal-form.jsx
@@ -1,29 +1,33 @@
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import { Form as RouterForm } from 'react-router-dom'
-import { formatDate } from '../utils/formatDate'
-import CancelButton from './cancel-button'
-import { useEffect, useRef } from 'react'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Form as RouterForm } from "react-router-dom";
+import { formatDate } from "../utils/formatDate";
+import CancelButton from "./cancel-button";
+import { useEffect, useRef } from "react";
 
 const goalNullObject = {
-  description: '',
+  description: "",
   targetDate: new Date().setDate(new Date().getDate() + 1),
   initialValue: 0,
   targetValue: 0,
-  interval: 'daily',
-}
+  interval: "daily",
+};
 
 export default function GoalForm({ goal = goalNullObject }) {
-  const ref = useRef()
+  const ref = useRef();
   useEffect(() => {
     if (ref.current) {
-      ref.current.focus()
+      ref.current.focus();
     }
-  }, [])
+  }, []);
 
   return (
     <Form as={RouterForm} method="post">
-      <Form.Group as="section" className="mb-3" controlId="goal-field-description">
+      <Form.Group
+        as="section"
+        className="mb-3"
+        controlId="goal-field-description"
+      >
         <Form.Label>Goal description</Form.Label>
         <Form.Control
           ref={ref}
@@ -39,40 +43,79 @@ export default function GoalForm({ goal = goalNullObject }) {
           Description must be at least 15 characters long
         </Form.Text>
       </Form.Group>
-      <Form.Group as="section" className="mb-3" controlId="goal-field-initial-value">
-        <Form.Label>Initial value</Form.Label>
-        <Form.Control type="number" min={0} name="initialValue" defaultValue={goal.initialValue} />
-      </Form.Group>
-      <Form.Group as="section" className="mb-3" controlId="goal-field-target-value">
-        <Form.Label>Target value</Form.Label>
-        <Form.Control required type="number" min={0} name="targetValue" defaultValue={goal.targetValue} />
-      </Form.Group>
-      <Form.Group as="section" className="mb-3" controlId="goal-field-target-date">
-        <Form.Label>Target date</Form.Label>
-        <Form.Control
-          required
-          type="date"
-          min={formatDate(new Date())}
-          name="targetDate"
-          defaultValue={formatDate(goal.targetDate)}
-          aria-describedby="target-date-help-block"
-        />
-        <Form.Text id="target-date-help-block" muted>
-          Target date must be in future
-        </Form.Text>
-      </Form.Group>
-      <Form.Group as="section" className="mb-3" controlId="goal-field-interval">
-        <Form.Label>Interval</Form.Label>
-        <Form.Select aria-label="Goal time frame interval" name="interval" defaultValue={goal.interval}>
-          <option value="daily">daily</option>
-          <option value="weekly">weekly</option>
-          <option value="monthly">monthly</option>
-        </Form.Select>
-      </Form.Group>
+      <div
+        style={{ display: "grid", gap: "1rem", gridTemplateColumns: "1fr 1fr" }}
+      >
+        <Form.Group
+          as="section"
+          className="mb-3"
+          controlId="goal-field-initial-value"
+        >
+          <Form.Label>Initial value</Form.Label>
+          <Form.Control
+            type="number"
+            min={0}
+            name="initialValue"
+            defaultValue={goal.initialValue}
+          />
+        </Form.Group>
+        <Form.Group
+          as="section"
+          className="mb-3"
+          controlId="goal-field-target-value"
+        >
+          <Form.Label>Target value</Form.Label>
+          <Form.Control
+            required
+            type="number"
+            min={0}
+            name="targetValue"
+            defaultValue={goal.targetValue}
+          />
+        </Form.Group>
+      </div>
+      <div
+        style={{ display: "grid", gap: "1rem", gridTemplateColumns: "2fr 1fr" }}
+      >
+        <Form.Group
+          as="section"
+          className="mb-3"
+          controlId="goal-field-target-date"
+        >
+          <Form.Label>Target date</Form.Label>
+          <Form.Control
+            required
+            type="date"
+            min={formatDate(goal.targetDate)}
+            name="targetDate"
+            defaultValue={formatDate(goal.targetDate)}
+            aria-describedby="target-date-help-block"
+          />
+          <Form.Text id="target-date-help-block" muted>
+            Target date must be in future
+          </Form.Text>
+        </Form.Group>
+        <Form.Group
+          as="section"
+          className="mb-3"
+          controlId="goal-field-interval"
+        >
+          <Form.Label>Interval</Form.Label>
+          <Form.Select
+            aria-label="Goal time frame interval"
+            name="interval"
+            defaultValue={goal.interval}
+          >
+            <option value="daily">daily</option>
+            <option value="weekly">weekly</option>
+            <option value="monthly">monthly</option>
+          </Form.Select>
+        </Form.Group>
+      </div>
       <Button variant="primary" type="submit" className="me-2">
         Save
       </Button>
       <CancelButton />
     </Form>
-  )
+  );
 }
