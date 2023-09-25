c0b80c42b100aeec8bae24677cfc67729957dbbe
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-25 19:42:26 +0300

    feat: add test case when created_at matches target_date

--- spec/services/goal_time_frame_calculation_service_spec.rb
+++ spec/services/goal_time_frame_calculation_service_spec.rb
@@ -19,6 +19,15 @@ RSpec.describe GoalTimeFrameCalculationService, type: :service do
       expect(time_frames).to eq((0..10).map { |i| goal.created_at.beginning_of_day + i.days })
     end
 
+    context 'when created_at matches target_date' do
+      it 'calculates time frames correctly' do
+        time = Time.current
+        goal = build_stubbed(:goal, created_at: time, target_date: time, interval: 'daily')
+        time_frames = service.call(goal)
+        expect(time_frames.count).to eq(1)
+      end
+    end
+
     it 'calculates time frames for daily interval' do
       goal = build_stubbed(:goal, target_date: 10.days.from_now, interval: 'daily')
       time_frames = service.call(goal)
