f88580a8e8d778479f9bc904788944c2a7fddc48
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-25 19:32:40 +0300

    fix: GoalTimeFrameCalculationService

--- app/services/goal_time_frame_calculation_service.rb
+++ app/services/goal_time_frame_calculation_service.rb
@@ -12,14 +12,15 @@ class GoalTimeFrameCalculationService < BaseService
   attr_reader :goal
 
   def calculate_time_frames
-    step = interval_to_step.fetch(goal.interval, 1.day)
+    step = interval_to_step.fetch(goal.interval)
     current_date = goal.created_at.beginning_of_day
-    time_frames = [current_date]
     target_date = goal.target_date.beginning_of_day
+    time_frames = []
 
-    while current_date < target_date
-      current_date += step
+    loop do
       time_frames << current_date
+      current_date += step
+      break if current_date > target_date
     end
 
     time_frames
@@ -27,9 +28,9 @@ class GoalTimeFrameCalculationService < BaseService
 
   def interval_to_step
     {
-      daily: 1.day,
-      weekly: 1.week,
-      monthly: 1.month
+      'daily' => 1.day,
+      'weekly' => 1.week,
+      'monthly' => 1.month
     }
   end
 end
