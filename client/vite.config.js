199f632d862918b90060e9cb7265762593ac8b76
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-09-26 18:30:35 +0300

    feat: install testing-library/react

--- client/vite.config.js
+++ client/vite.config.js
@@ -21,5 +21,10 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.js",
+    },
   };
 });
