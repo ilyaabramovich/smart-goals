b5adadc8af5c2bf8de81ab5ba31e0b5c28901ffe
ilyaabramovich <ilyaabramovich1998@gmail.com>
2023-10-13 20:22:58 +0300

    style: fix line length

--- app/serializers/goal_details_serializer.rb
+++ app/serializers/goal_details_serializer.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class GoalDetailsSerializer < ActiveModel::Serializer
-  attributes :id, :description, :target_date, :target_value, :initial_value, :interval, :accumulated_value, :completion_percentage, :days_to_complete
+  attributes :id,
+    :description,
+    :target_date,
+    :target_value,
+    :initial_value,
+    :interval,
+    :accumulated_value,
+    :completion_percentage,
+    :days_to_complete
 
   attribute :stats_total
 
